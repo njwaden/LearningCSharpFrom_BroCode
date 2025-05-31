27a69150708042a54e31621af0b9ddc821d33651
Najwa Najib Bamadhaj <najwanajib2000@gmail.com>
2025-05-31 18:53:53 +0800

    added chapter 16

---
+++ 16_ForLoops/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _16_ForLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //for loop = repeats some code a FINITE amount of times
+            /*
+            for (int i = 1; i <= 10; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+            */
+
+            for (int i = 10; i > 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Happy New Year!!");
+
+            Console.ReadKey();
+        }
+    }
+}
