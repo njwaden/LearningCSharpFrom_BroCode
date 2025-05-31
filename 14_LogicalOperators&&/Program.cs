503472cae664e770108433bc10bae98064aa7048
Najwa Najib Bamadhaj <najwanajib2000@gmail.com>
2025-05-31 18:35:42 +0800

    added for chapter 14

---
+++ 14_LogicalOperators&&/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _14_LogicalOperators__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //logical operators = can be used to check if more than 1 condition is true/false
+            //&& = AND operator, || = OR operator, ! = NOT operator
+
+            Console.WriteLine("What's the temperature outside: (C)");
+            double temp = Convert.ToDouble(Console.ReadLine());
+
+            if (temp >= 10 && temp <= 25)
+            {
+                Console.WriteLine("It's warm outside!");
+            }
+
+            else if (temp <= 50 || temp >= 50)
+            {
+                Console.WriteLine("DO NOT GO OUTSIDE!");
+            }
+
+                Console.ReadKey();
+        }
+    }
+}
