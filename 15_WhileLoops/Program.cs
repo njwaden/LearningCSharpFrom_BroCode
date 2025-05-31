24af5f35205e049edcafeeb065980276ab738c98
Najwa Najib Bamadhaj <najwanajib2000@gmail.com>
2025-05-31 18:45:51 +0800

    added for chapter 15

---
+++ 15_WhileLoops/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _15_WhileLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //while loop = repeats some code while some condition remains true
+
+            Console.Write("Enter your name: ");
+            String name = Console.ReadLine();
+
+            while (name == "")                  //needs something to exit the while loop or else it will be infinite loop
+            {
+                Console.Write("Enter your name: ");
+                name = Console.ReadLine();
+            }
+
+            Console.WriteLine("Hello " + name);
+
+            Console.ReadKey();
+        }
+    }
+}
