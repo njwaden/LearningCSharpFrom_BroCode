62cbbbbd25f19da76e99f300428a965d49c08c7e
Najwa Najib Bamadhaj <najwanajib2000@gmail.com>
2025-05-31 10:11:58 +0800

    updated chap 10 added chap 11

--- 10_HypotenuseCalculatorProgram/Program.cs
+++ 10_HypotenuseCalculatorProgram/Program.cs
@@ -2,7 +2,15 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Enter side A: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter side B: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        double c = Math.Sqrt((a * a) + (b * b));
+
+        Console.WriteLine("The hypotaneuse is: " + c);
 
         Console.ReadKey();
     }
---
+++ 11_StringMethods/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        String fullName = "nasibukhari";
+        String phoneNum = "123-456-7890";
+
+        //fullName = fullName.ToUpper();
+        //fullName = fullName.ToLower();
+        //Console.WriteLine(fullName);
+
+        //phoneNum = phoneNum.Replace("-","");
+        //Console.WriteLine(phoneNum);
+
+        //String username = fullName.Insert(0,"@");       //first letter is at index 0 so @ will be inserted at the start of the string
+        //Console.WriteLine(username);
+
+        //Console.WriteLine(fullName.Length); 
+
+        String firstName = fullName.Substring(0,4);         // substring(startIndex, length)
+        String lastName = fullName.Substring(4, 7);
+
+        Console.WriteLine("Your first name: " + firstName);
+        Console.WriteLine("Your last name: " + lastName);
+
+        Console.ReadKey();
+    }
+}
