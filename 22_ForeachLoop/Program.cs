4169d9c52c4efae5ca8eb8f4697a036e93978aed
Najwa Najib Bamadhaj <najwanajib2000@gmail.com>
2025-06-01 20:42:51 +0800

    added chapter 22

---
+++ 22_ForeachLoop/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _22_ForeachLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //continue from chap 21:
+
+            // foreach looop = a simpler way to iterate over an array, but it's less flexible
+
+            String[] cars = {"BMW", "Mustang", "Corvette"};
+
+            foreach (String car in cars)                        // car is the nickname of the element we're working with
+            {                                                   // foreach loop is not flexible. for loop can adjust the iterations and such.
+                Console.WriteLine(car);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
