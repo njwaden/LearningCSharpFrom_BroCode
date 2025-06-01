213479aad2e3c9311daa661fb994973f089707f4
Najwa Najib Bamadhaj <najwanajib2000@gmail.com>
2025-06-01 21:07:55 +0800

    added chap 23

---
+++ 23_Methods/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _23_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //method = performs a section of code, whenever it's called "invoked".
+            // benefit = Let's us reuse code without writing it multiple times.
+
+            String name = "sayang";                        //arguments = what is sent to methods
+            int age = 26;
+            singHappyBirthday(name, age);                            
+            
+            Console.ReadKey();
+        }
+
+        static void singHappyBirthday(String birthdayGIRL, int yearsOld)     //parameters = what a method needs to receive to be invoked. parameters doesnt necessarily use the same name as the arguments we're passing.                       
+                                                                //methods do not have access to any variables on another method
+        {
+            Console.WriteLine("Happy birthday to you!");
+            Console.WriteLine("Happy birthday to you!");
+            Console.WriteLine("Happy birthday dear " + birthdayGIRL + "!");
+            Console.WriteLine("You are " + yearsOld + " years old! :3");
+            Console.WriteLine("Happy birthday to you!");
+            Console.WriteLine();
+        }
+    }
+}
