12700834dd90cc78cf7b704d72262ec0ac6c6f9c
Najwa Najib Bamadhaj <najwanajib2000@gmail.com>
2025-06-01 20:35:39 +0800

    added chapter 21

---
+++ 21_Arrays/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _21_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //array = variable that can store multiple values. fixed size
+
+            String[] cars = new string[3];
+
+            //String[] cars = { "BMW", "Mustang", "Corvette" };
+
+            cars[0] = "Tesla";
+
+            for (int i = 0; i < cars.Length; i++)
+            {
+                Console.WriteLine(cars[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
