d44c72c816d46011a5d0e4892a2bcf53db148ef9
Najwa Najib Bamadhaj <najwanajib2000@gmail.com>
2025-06-01 21:17:36 +0800

    added chapter 24

---
+++ 24_ReturnKeywords/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _24_ReturnKeywords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // return = returns data back to the place where a method is invoked
+
+            double x;
+            double y;
+            double result;
+            Console.WriteLine("Enter in number 1: ");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter in number 2: ");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            result = Multiply(x, y);
+
+            Console.WriteLine("Result = " + result);
+
+
+            Console.ReadKey();
+        }
+
+        static double Multiply(double x, double y)          //keyword "void" changed to "double" because that's the data type of what being return to.
+        {
+            double z = x * y;
+            return z;
+        }
+    }
+}
