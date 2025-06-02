b3f1f10b8ef12365306a14cefc4f5397b9a49276
Najwa Najib Bamadhaj <najwanajib2000@gmail.com>
2025-06-02 20:35:30 +0800

    added chapter 32

---
+++ 32_Objects/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _32_Objects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //object = an instance of a class
+            //         a class can be used as a blueprint to create object (OOP)
+            //         object can have fields & methods (characteristics & actions)
+
+            Human human1 = new Human();             //human object in human1
+            Human human2 = new Human();
+
+            human1.name = "nasibukhari";
+            human1.age = 25;
+
+            human2.name = "cimerm";
+            human2.age = 26;
+
+            human1.Eat();
+            human1.Sleep();
+
+            human2.Eat();
+            human2.Sleep();
+
+            Console.ReadKey();
+        }
+    }
+
+    class Human()
+    {
+        public String name;
+        public int age;
+
+        public void Eat()
+        {
+            Console.WriteLine(name + " is eating");
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine(name + " is sleeping");
+        }
+    }
+}
