e8896dfc3ebd3a67432354640832c4a5374623bf
Najwa Najib Bamadhaj <najwanajib2000@gmail.com>
2025-06-02 20:14:58 +0800

    added chap 30

---
+++ 30_MultidimensionalArrays/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _30_MultidimensionalArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //multidimensional array = an array of arrays. good to use for matrix, grid, tables of data
+
+            String[] ford = { "Mustang", "F-150", "Explorer" };
+            String[] chevy = { "Corvetter", "Camaro", "Silverado" };
+            String[] toyota = { "Corolla", "Camry", "Rav4" };
+
+            String[,] parkingLot = { { "Mustang", "F-150", "Explorer" }, 
+                                     { "Corvetter", "Camaro", "Silverado" } , 
+                                     { "Corolla", "Camry", "Rav4" } 
+                                   };
+
+            parkingLot[0, 2] = "Fusion";            //0 row, 2nd column
+            parkingLot[2, 0] = "Tacoma";
+
+            /*
+            foreach (String car in parkingLot)
+            {
+                Console.WriteLine(car);
+            }
+            */
+
+            for (int i = 0; i < parkingLot.GetLength(0); i++)
+            {
+                for (int j = 0; j < parkingLot.GetLength(1); j++)
+                {
+                    Console.Write(parkingLot[i,j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
