41b8b37752260fd887080d6135810a508830f7cd
Najwa Najib Bamadhaj <najwanajib2000@gmail.com>
2025-06-02 23:30:51 +0800

    added chap 40

---
+++ 40_MethodOverriding/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _40_MethodOverriding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //method overriding = provides a new verson of a method inherited from a parent class
+            //                    inherited method must be: abstract, virtual or already overridden
+            //                    used with ToString(), polymorphism
+
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            dog.Speak();
+            cat.Speak();
+
+            Console.ReadKey();
+        }
+    }
+
+    class Animal
+    {
+        public virtual void Speak()
+        {
+            Console.WriteLine("The animal goes *brrrrr*");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public override void Speak()
+        {
+            Console.WriteLine("The dog goes *woof*");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public override void Speak()
+        {
+            Console.WriteLine("The cat goes *meow*");
+
+        }
+    }
+}
