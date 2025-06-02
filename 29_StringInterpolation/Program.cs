55ae44eb20ffc240ea3833c7d87328c5509083e1
Najwa Najib Bamadhaj <najwanajib2000@gmail.com>
2025-06-02 15:18:44 +0800

    added chap 29

---
+++ 29_StringInterpolation/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _29_StringInterpolation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string interpolation = allows us to insert variables into a string literal
+            //                       precede a string literal with $
+            //                       {} are placeholders
+
+            String firstName = "nasi";
+            String lastName = "bukhari";
+            int age = 25;
+
+            //Console.WriteLine("Hello " + firstName+ " " + lastName + ".");
+            //Console.WriteLine("You are " + age + " years old.");
+
+            Console.WriteLine($"Hello {firstName} {lastName}.");
+            Console.WriteLine($"You are {age,10} years old.");              //10 is the width of the field, it will align the text to the right
+
+            Console.ReadKey();
+        }
+    }
+}
