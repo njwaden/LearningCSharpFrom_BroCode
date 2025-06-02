6064c309ff276030a0357af4332d51016b7d2116
Najwa Najib Bamadhaj <najwanajib2000@gmail.com>
2025-06-02 23:41:06 +0800

    added  chap 41

---
+++ 41_ToStringMethod/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _41_ToStringMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ToString() = converts an object to its string representation so that it is suitable for display
+
+            Car car = new Car("Chevy", "Corvette", 2022, "blue");
+
+            Console.WriteLine(car);
+
+            Console.ReadKey();
+        }
+    }
+
+    class Car
+    {
+        String make;
+        String model;
+        int year;
+        String colour;
+
+        public Car(String make, String model, int year, String colour)
+        {
+            this.make = make;
+            this.model = model;
+            this.year = year;
+            this.colour = colour;
+        }
+
+        public override string ToString()
+        {
+            return "This is a " + make + " " + model;
+        }
+    }
+}
