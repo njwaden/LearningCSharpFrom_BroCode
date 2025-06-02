40be5fc3fa47b0a978c500e9c0f1dbff17e0ccf2
Najwa Najib Bamadhaj <najwanajib2000@gmail.com>
2025-06-02 23:22:27 +0800

    added chap 39

---
+++ 39_ObjectsAsArguments/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _39_ObjectsAsArguments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Car car1 = new Car("Mustang", "red");
+
+            Car car2 = Copy(car1);
+
+            Console.WriteLine(car2.colour + " " + car2.model);
+
+            Console.ReadKey();
+        }
+
+        public static Car Copy(Car car)
+        {
+            return new Car(car.model, car.colour);
+        }
+    }
+
+    class Car
+    {
+
+        public String model;
+        public String colour;
+
+        public Car (String model, String colour)
+        {
+            this.model = model;
+            this.colour = colour;
+        }
+    }
+}
