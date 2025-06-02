3a0a39d36743da8c01fa2de83bccd10406752bc6
Najwa Najib Bamadhaj <najwanajib2000@gmail.com>
2025-06-02 23:03:23 +0800

    added chapter 37

---
+++ 37_AbstractClasses/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _37_AbstractClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //abstract class = modifier that indicates missing components or incomplete implementation
+
+            Car car = new Car();
+            Bicycle bicycle = new Bicycle();
+            Boat boat = new Boat();
+            //Vehicle vehicle = new Vehicle();
+
+            Console.ReadKey();
+        }
+    }
+    abstract class Vehicle
+    {
+        public int speed = 0;
+
+        public void Go()
+        {
+            Console.WriteLine("This vehicle is moving!");
+        }
+    }
+
+    class Car : Vehicle                         //Car is the child class, Vehicle is the parent class
+    {
+        public int wheels = 4;
+        int maxSpeed = 500;
+    }
+
+    class Bicycle : Vehicle                         //Bicycle is the child class, Vehicle is the parent class
+    {
+        public int wheels = 2;
+        int maxSpeed = 50;
+    }
+
+    class Boat : Vehicle                         //Boat is the child class, Vehicle is the parent class
+    {
+        public int wheels = 0;
+        int maxSpeed = 100;
+    }
+}
