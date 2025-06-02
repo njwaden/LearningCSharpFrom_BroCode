ecd4a1e83054068443af3d45aa045b28612dd9cc
Najwa Najib Bamadhaj <najwanajib2000@gmail.com>
2025-06-02 20:25:22 +0800

    added chapter 31

---
+++ 31_Classes/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _31_Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //class = A bundle of related code.
+            //        can be used as a blueprint to create object (OOP)
+
+            Messages.Hello();
+            Messages.Waiting();
+            Messages.Bye();
+
+            Console.ReadKey();
+        }
+    }
+
+}
