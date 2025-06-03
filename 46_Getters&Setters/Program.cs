cf675993bcaff61a4bc5fa644631ac44e532a5b8
Najwa Najib Bamadhaj <najwanajib2000@gmail.com>
2025-06-03 10:45:42 +0800

    added chapter 46

---
+++ 46_Getters&Setters/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace _46_Getters_Setters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //getters & setters = add security to fields by encapsulation
+            //                    they're acessors found within properties
+
+            //properties =  combine aspects of both fields and methods (share name with a field)
+            //get accessor = used to return the property value
+            //get accessor = used to assign a new value
+            //value keyword = defines the value being assigned by the set (parameter)
+
+            Car car = new Car(400);
+
+            car.Speed = 1000000;          //how to prevent programmer from changing the speed directly as its going WAY too fast
+
+            Console.WriteLine(car.Speed);
+
+            Console.ReadKey();
+        }
+    }
+
+    class Car
+    {
+        public int speed;
+
+        public Car (int speed)
+        {
+            Speed = speed;
+        }
+
+        public int Speed
+        {
+            get { return speed; }       //read
+            set                         //writable
+            {
+                if (value > 500)
+                {
+                    speed = 500;
+                }
+                else
+                {
+                    speed = value;      //write
+                }
+                    
+            }
+        }
+    }
+}
