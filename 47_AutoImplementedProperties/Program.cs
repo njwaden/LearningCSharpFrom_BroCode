8486e7f70ebe5f21da6ee9ea9da6b35bfed5cbea
Najwa Najib Bamadhaj <najwanajib2000@gmail.com>
2025-06-03 10:53:02 +0800

    added chapter 47

---
+++ 47_AutoImplementedProperties/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _47_AutoImplementedProperties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //auto-implemented properties = shortcut when no additional logic is required in the property
+            //                              you do not have to define a field for a property,
+            //                              you only have to write get; and/or set; inside the property
+
+            Car car = new Car("Porche");
+
+            Console.WriteLine(car.Model);
+
+            Console.ReadKey();
+        }
+    }
+
+    class Car
+    {
+        public String Model { get; set; }       //can use this when there's no additional logic besides setting the value and getting the value
+
+        public Car (String model)
+        {
+            this.Model = model;
+        }
+    }
+}
