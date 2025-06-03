2866f1ffa8c138e4669f01640572899984c82e75
Najwa Najib Bamadhaj <najwanajib2000@gmail.com>
2025-06-03 10:32:10 +0800

    added chapter 45

---
+++ 45_ListOfObjects/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _45_ListOfObjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Player> players = new List<Player>();
+
+            Player player1 = new Player("Chad");
+            Player player2 = new Player("Steve");
+            Player player3 = new Player("Karen");
+
+            players.Add(new Player("John"));
+            players.Add(player2);
+            players.Add(player3);
+
+            foreach (Player player in players)
+            {
+                //Console.WriteLine(player.username);
+                Console.WriteLine(player);
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    class Player
+    {
+        public String username;
+
+        public Player (String username)
+        {
+            this.username = username;
+        }
+
+        public override string ToString()
+        {
+            return username;
+        }
+    }
+}
